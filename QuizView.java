6dcebbf425e219ee7ecf3290bb0e06ec235de1a1
henry226 <henry5581984@gmail.com>
2018-11-27 16:02:10 -0500

    added quiz view

---
+++ QuizView.java
@@ -0,0 +1,105 @@
+import java.io.IOException;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class QuizView {
+	
+	static Stage subStage;
+	
+	QuizView() {
+		subStage = new Stage();
+		subStage.setTitle("Quiz Game");
+
+		AnchorPane anchorPane = new AnchorPane();
+		anchorPane.setPrefWidth(650);
+		anchorPane.setPrefHeight(343);
+
+		Pane pane = new Pane();
+		pane.setPrefWidth(650);
+		pane.setPrefHeight(372);
+		pane.setStyle("-fx-background-color:#fff;");
+
+		Text questionNo = new Text("Question 1:");
+		questionNo.setLayoutX(31);
+		questionNo.setLayoutY(56);
+		questionNo.setStyle("-fx-font-size:20;");
+
+		Text questions = new Text("questions");
+		questions.setLayoutX(138);
+		questions.setLayoutY(56);
+		questions.setStyle("-fx-font-size:19;");
+
+		RadioButton optionA = new RadioButton("A )");
+		optionA.setLayoutX(50);
+		optionA.setLayoutY(112);
+		optionA.setStyle("-fx-font-size:16;");
+
+		RadioButton optionB = new RadioButton("B )");
+		optionB.setLayoutX(50);
+		optionB.setLayoutY(151);
+		optionB.setStyle("-fx-font-size:16;");
+
+		RadioButton optionC = new RadioButton("C )");
+		optionC.setLayoutX(50);
+		optionC.setLayoutY(191);
+		optionC.setStyle("-fx-font-size:16;");
+
+		RadioButton optionD = new RadioButton("D )");
+		optionD.setLayoutX(50);
+		optionD.setLayoutY(231);
+		optionD.setStyle("-fx-font-size:16;");
+
+		Button nextQuestion = new Button("Save & Continue");
+		nextQuestion.setLayoutX(50);
+		nextQuestion.setLayoutY(299);
+		nextQuestion.setPrefWidth(155);
+		nextQuestion.setPrefHeight(35);
+
+		Button finishQuiz = new Button("Finish Quiz");
+		finishQuiz.setLayoutX(450);
+		finishQuiz.setLayoutY(299);
+		finishQuiz.setPrefWidth(155);
+		finishQuiz.setPrefHeight(35);
+
+		pane.getChildren().addAll(questionNo, questions, optionA, optionB, optionC, optionD, nextQuestion, finishQuiz);
+
+		Scene scene = new Scene(anchorPane);
+		scene.getStylesheets().add(getClass().getResource("/CSS/main.css").toExternalForm());
+		anchorPane.getChildren().add(pane);
+
+		/*JavaQuizController quiz = new JavaQuizController();
+		quiz.setJavaQuizController(b1, b2, b3, b4, qNo, questions, save_cont, finish);
+		save_cont.setOnAction(e -> {
+			try {
+
+				quiz.btnNext();
+
+			} catch (IOException e1) {
+
+				e1.printStackTrace();
+			}
+		});
+
+		finish.setOnAction(e -> {
+			try {
+				quiz.setDialogBox();
+				subStage.hide();
+			} catch (IOException e1) {
+
+				e1.printStackTrace();
+			}
+		});*/
+
+		subStage.setScene(scene);
+		subStage.show();
+	}
+
+	/*public static void setStage_hide() {
+		QuizView.subStage.hide();
+	}*/
+}
